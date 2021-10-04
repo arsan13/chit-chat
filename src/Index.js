79ea7fda113ad0da2781f8578789a8c7a20eb8ea
arsan13 <arsan.anbu97@gmail.com>
2021-10-04 09:07:38 +0530

    Added Design for Login and Register

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
+import {Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MessagesScreen from './screens/MessagesScreen';
 import ChatScreen from './screens/ChatScreen';
 import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +15,26 @@ const Index = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {!isLoggedIn && <Stack.Screen name="Login" component={LoginScreen} />}
-        <Stack.Screen name="Messages" component={MessagesScreen} />
+        {!isLoggedIn && (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </>
+        )}
+        <Stack.Screen
+          name="Messages"
+          component={MessagesScreen}
+          options={{
+            headerRight: () => (
+              <Button
+                title="logout"
+                onPress={() => {
+                  setIsLoggedIn(false);
+                }}
+              />
+            ),
+          }}
+        />
         <Stack.Screen
           name="Chat"
           component={ChatScreen}
